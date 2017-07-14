a29968c03da39ec6ab3f57bfbe0d295dbdf2af3f
pukkamustard <adarsh@posteo.net>
2017-07-14 16:33:09 +0200

    Update assets and allow custom asset directory.

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ const path = require('path')
 const argv = require('minimist')(process.argv.slice(2))
 
 if (require.main === module) {
-  const externalPath = argv._[0] || '.'
+  const root = path.resolve(argv._[0] || '.')
+  console.log('Starting Play[dev] in folder:', root)
 
-  app.use('/root', express.static(externalPath))
-  console.log(externalPath)
+  app.use('/root', express.static(root))
 
-  app.use(express.static(path.join(__dirname, 'assets')))
+  var assets = path.join(__dirname, 'assets')
+  if (argv['a']) {
+    assets = path.resolve(argv['a'])
+  }
+  app.use(express.static(assets))
+
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(assets, 'playDev.html'))
+  })
 
   app.listen(8388, function () {
     console.log('http://localhost:8388/')
