3ae1234678316ce95243a2749507af9789d49764
pukkamustard <adarsh@posteo.net>
2017-07-18 23:18:38 +0200

    Proxy to Play runtime instead of bundling and self-hosting

--- index.js
+++ index.js
@@ -5,22 +5,17 @@ const fs = require('fs')
 const express = require('express')
 const app = express()
 
+const proxy = require('http-proxy-middleware')
+
 const opn = require('opn')
 
 const argv = require('minimist')(process.argv.slice(2))
 
 async function startServer () {
+  // /root - where the game in developement is hosted
   app.use('/root', await rootMiddleware())
-
-  var assets = path.join(__dirname, 'assets')
-  if (argv['a']) {
-    assets = path.resolve(argv['a'])
-  }
-  app.use(express.static(assets))
-
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(assets, 'playDev.html'))
-  })
+  // everything else is assets that are proxied from Dividat Play
+  app.use(assetsMiddleware())
 
   app.listen(8388, function () {
     console.log('http://localhost:8388/')
@@ -28,11 +23,24 @@ async function startServer () {
   })
 }
 
+function assetsMiddleware () {
+  // Dividat developers only: use the '-l' flag to use local development assets
+  const assetsLocation = argv['l'] ? 'http://localhost:8080/' : 'https://play.dividat.com/'
+  return proxy({
+    target: assetsLocation,
+    pathRewrite: {
+      '^/$': '/playDev.html'
+    },
+    changeOrigin: true,
+    logLevel: 'warn'
+  })
+}
+
 function rootMiddleware () {
   const root = argv._[0] || '.'
-  // Check if folder exists
   return new Promise((resolve, reject) => {
     const resolvedRoot = path.resolve(root)
+    // Check if folder exists
     fs.access(resolvedRoot, fs.constants.F_OK | fs.constants.R_OK, (err) => {
       if (err) {
         // root is an url
